0ff4f342db6c8d1faf0512b559160d8ca8d6b344
gitknilch <knilch0r@users.noreply.github.com>
2013-03-03 23:43:55 +0100

    prepare for adding hiscore. unfortunately, this looks like it's going to be a huge load of ugly copy-and-paste stuff.
    Change-Id: I494cb8baa02793986901cf2154ebb77262ad508e
    Signed-off-by: gitknilch <gitknilch@cwde.de>

---
+++ src/de/cwde/shisensho/HighscoreActivity.java
@@ -0,0 +1,18 @@
+package de.cwde.shisensho;
+
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+
+public class HighscoreActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.highscore);
+	}
+
+	public void clearHiscore(View view) {
+		// TODO
+	}
+}
