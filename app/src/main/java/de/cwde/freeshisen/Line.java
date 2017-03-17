76afe91f8c61d30cdc3fb9a159e54041e50d2e3b
knilch <knilch0r@users.noreply.github.com>
2017-03-17 10:14:53 +0100

    replace getMin/Max, isVertical/Horizontal by final attributes, add isPoint

--- app/src/main/java/de/cwde/freeshisen/Line.java
+++ app/src/main/java/de/cwde/freeshisen/Line.java
@@ -8,55 +8,50 @@
 
 package de.cwde.freeshisen;
 
-public class Line {
-	public Point a;
-	public Point b;
+class Line {
+	final Point a;
+	final Point b;
+	private final boolean isHorizontal;
+	private final boolean isVertical;
+	private final Point Min;
+	private final Point Max;
 
-	public Line(Point a, Point b) {
+	Line(Point a, Point b) {
 		this.a = a;
 		this.b = b;
+		isHorizontal = (a.i == b.i);
+		isVertical = (a.j == b.j);
+		if (a.i < b.i || a.j < b.j) {
+			Min = a;
+			Max = b;
+		} else {
+			Min = b;
+			Max = a;
+		}
 	}
 
-	public boolean equals(Line l) {
+	boolean equals(Line l) {
 		return (a.equals(l.a) && b.equals(l.b));
 	}
 
-	public boolean contains(Point p) {
-		return (p.i == a.i && p.i == b.i && p.j >= getMin().j && p.j <= getMax().j)
-				|| (p.j == a.j && p.j == b.j && p.i >= getMin().i && p.i <= getMax().i);
+	boolean contains(Point p) {
+		return (isHorizontal && p.i == a.i && p.j >= Min.j && p.j <= Max.j)
+				|| (isVertical && p.j == a.j && p.i >= Min.i && p.i <= Max.i);
 	}
 
-	public Point getMin() {
-		if (a.i < b.i || a.j < b.j) return a;
-		else return b;
-	}
-
-	public Point getMax() {
-		if (a.i > b.i || a.j > b.j) return a;
-		else return b;
-	}
-
-	public Point cuts(Line l) {
-		if (isHorizontal() && l.isVertical()
-				&& getMin().j <= l.a.j && getMax().j >= l.a.j
-				&& l.getMin().i <= a.i && l.getMax().i >= a.i) {
+	Point cuts(Line l) {
+		if (isHorizontal && l.isVertical
+				&& Min.j <= l.a.j && Max.j >= l.a.j
+				&& l.Min.i <= a.i && l.Max.i >= a.i) {
 			return new Point(a.i, l.a.j);
-		} else if (isVertical() && l.isHorizontal()
-				&& getMin().i <= l.a.i && getMax().i >= l.a.i
-				&& l.getMin().j <= a.j && l.getMax().j >= a.j) {
+		} else if (isVertical && l.isHorizontal
+				&& Min.i <= l.a.i && Max.i >= l.a.i
+				&& l.Min.j <= a.j && l.Max.j >= a.j) {
 			return new Point(l.a.i, a.j);
 		} else return null;
 	}
 
-	public boolean isHorizontal() {
-		return (a.i == b.i);
-	}
-
-	public boolean isVertical() {
-		return (a.j == b.j);
-	}
-
-	public String toString() {
-		return a + "-" + b;
+	boolean isPoint() {
+		return (a.i == b.i && a.j == b.j);
 	}
 }
